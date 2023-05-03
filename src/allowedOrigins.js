64ca5e6cbb85e6387482d5d1c8839117664601b9
Van Vincent Vasquez <vanvasquez0725@gmail.com>
2023-05-03 18:50:19 +0800

    Update allowedOrigins.js

--- src/allowedOrigins.js
+++ src/allowedOrigins.js
@@ -1,1 +1,7 @@
-module.exports = ['http://localhost:3000'];
+module.exports = [
+  'http://localhost:3000',
+  'https://warranty-tracking-system-bw2aocjaf-vanvasquez.vercel.app',
+  'https://warranty-tracking-system-git-main-vanvasquez.vercel.app',
+  'https://warranty-tracking-system.vercel.app',
+  'https://warranty-tracking-system-vanvasquez.vercel.app',
+];
