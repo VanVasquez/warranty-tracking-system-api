9e183d223331c6a705194e38659c9d453ce13c8d
Van Vincent Vasquez <vanvasquez0725@gmail.com>
2023-05-04 19:35:49 +0800

    logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,4 +10,11 @@ module.exports = {
       });
     }
   },
+  Logout: async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies.jwt) return res.status(204).json();
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    res.status(204).json();
+  },
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const controller = require('../controllers');
 
 router.get('/all', controller.user.GetAllUsers);
-
+router.get('/logout', controller.user.Logout);
 module.exports = router;
